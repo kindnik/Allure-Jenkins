a918474d6c0a6b99490f92a674011091a9139ec5
User <nikita011294@mail.ru>
2022-09-16 11:55:35 +0300

    Allure+Jenkins_2

--- README.md
+++
@@ -1,8 +0,0 @@
-
-# UI Selenide автотесты находятся в пакете AppleInsider и Hh
-Разобран пример как работать с элементами в Selenide по паттерну PageObject
-Папка - appleInsider 
-Папка - core
-
-# Чтение настроек из файла .property и .conf в пакете readProperties
-Разобраны варианты чтения настроек из различных файлов
--- src/test/java/appleInsider/KursApache.java
+++ src/test/java/appleInsider/KursApache.java
@@ -10,7 +10,7 @@ public class KursApache {
     public KursApache() {
     }
 
-    SelenideElement buttonЗаписаться_курс = $x("//div[@class='course-detail__offer']//span[text()='Записаться на курс']");
+    SelenideElement buttonЗаписаться_курс = $x("//div[@class='course-detail__offer']//span[text()='Записаться на курс2222']");
 
     protected Записаться_на_курс clickButton() {
         buttonЗаписаться_курс.click();
